ca8c364c733518c0c065c19d9c4229aa9021c81a
euphobyte <56632888+euphobyte@users.noreply.github.com>
2024-10-07 19:51:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onload = function init(){
+window.onload = (event) => {
   alert(location.pathname.substring(location.pathname.lastIndexOf("/") + 1));
   window.document.title = "Web Editor";
 };
